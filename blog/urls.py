fd1ac2122d46511ab1ae729bfa0289961280298f
Erik <erik@theconditionalpixel.nl>
2017-02-22 15:43:47 +0100

    added create view for post objects

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.MainView.as_view(), name='main'),
     url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.NewPostFormView.as_view(), name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, DetailView
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 class MainView(TemplateView):
@@ -8,7 +12,7 @@ class MainView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.exclude(date_time_published__isnull=True)
+        context['posts'] = Post.objects.exclude(date_time_published__isnull=True).order_by('-date_time_published')
         return context
 
 class PostDetailView(DetailView):
@@ -16,7 +20,18 @@ class PostDetailView(DetailView):
     template_name = 'post.html'
 
     def get_context_data(self, **kwargs):
-        pk = self.kwargs['pk']
         context = super(PostDetailView, self).get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Post, pk = pk)
         return context
+
+class NewPostFormView(CreateView):
+    template_name = 'new_post.html'
+    form_class = PostForm
+    model = Post
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.date_time_published = timezone.now()
+        return super(NewPostFormView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('main')
