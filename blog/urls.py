c59c8cf414b5a75a82a1e0fe44b0db50aec9e228
Erik <erik@Mac-mini-van-The-Conditional-Pixel-2.local>
2017-02-22 13:23:17 +0100

    added post detail links

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.MainView.as_view(), name='main'),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -9,4 +9,14 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data(**kwargs)
         context['posts'] = Post.objects.exclude(date_time_published__isnull=True)
+        return context
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post.html'
+
+    def get_context_data(self, **kwargs):
+        pk = self.kwargs['pk']
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['post'] = get_object_or_404(Post, pk = pk)
         return context
