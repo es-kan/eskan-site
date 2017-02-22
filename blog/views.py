e66fb13f2dfe84e80957759ea308b4b7b754d758
Erik <erik@Mac-mini-van-The-Conditional-Pixel-2.local>
2017-02-22 10:54:00 +0100

    objects filtered to template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Post
 
 # Create your views here.
 class MainView(TemplateView):
     template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.exclude(date_time_published__isnull=True)
+        return context
