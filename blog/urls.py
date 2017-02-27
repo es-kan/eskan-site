cc9573be5de9def1dcc5cb144d1bfd3865bf5641
Erik <erik@theconditionalpixel.nl>
2017-02-27 16:59:45 +0100

    added post editing

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.MainView.as_view(), name='main'),
     url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.NewPostFormView.as_view(), name='post_new'),
+    url(r'post/edit/(?P<pk>\d+)/$', views.PostEditView.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
 from django.views.generic import TemplateView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+
 class MainView(TemplateView):
     template_name = 'main.html'
 
@@ -14,6 +13,7 @@ class MainView(TemplateView):
         context = super(MainView, self).get_context_data(**kwargs)
         context['posts'] = Post.objects.exclude(date_time_published__isnull=True).order_by('-date_time_published')
         return context
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -23,15 +23,26 @@ class PostDetailView(DetailView):
         context = super(PostDetailView, self).get_context_data(**kwargs)
         return context
 
+
 class NewPostFormView(CreateView):
-    template_name = 'new_post.html'
+    template_name = 'post_edit.html'
     form_class = PostForm
     model = Post
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.date_time_published = timezone.now()
+        form.instance.date_time_published = timezone.localtime(timezone.now())
         return super(NewPostFormView, self).form_valid(form)
 
     def get_success_url(self):
-        return reverse('main')
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostEditView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'text']
+
+    def get_object(self):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return post
